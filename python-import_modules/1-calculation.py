e443766b4e7008274ef562d2940f57591d95c21c
OkanMahmudov <mahmudovokan47@gmail.com>
2025-05-10 15:42:44 +0400

    asdfghjytgdfg

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, div, mul
+
 if __name__ == "__main__":
     a = 10
     b = 5
@@ -7,4 +8,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-    
