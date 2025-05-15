29090018f40cb5ba70c90a0bfacdca6393d8dfd8
OkanMahmudov <mahmudovokan47@gmail.com>
2025-05-15 17:08:00 +0400

    sdfghjhg

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10, 10)  # Случайное число от -10 до 10
+number = random.randint(-10, 10)
 
-# Допишите код для проверки значения переменной number
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
+
